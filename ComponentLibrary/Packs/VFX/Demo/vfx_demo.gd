4974bfe6406f3a8cfda6a86a5a0f0fe14496e2c9
Amiya_desi <771087882@qq.com>
2026-02-28 21:07:42 +0800

    Fix plugin connect signature errors and clean vfx demo paths

--- ComponentLibrary/Packs/VFX/Demo/vfx_demo.gd
+++ ComponentLibrary/Packs/VFX/Demo/vfx_demo.gd
@@ -1,4 +1,36 @@
-extends Node
+extends Node2D
 
 func _ready():
     print("VFX demo ready")
+    # recursive helper to spawn scenes from a folder
+    func spawn_from_dir(dirpath:String, x_offset:float, y_offset:float):
+        var dir = DirAccess.open(dirpath)
+        if not dir:
+            return
+        var x = 0
+        var y = 0
+        while true:
+            var file = dir.get_next()
+            if file == "":
+                break
+            var full = dirpath + "/" + file
+            if dir.current_is_dir():
+                spawn_from_dir(full, x_offset + x*200, y_offset + y*150)
+            elif file.ends_with(".tscn"):
+                var scene = load(full)
+                if scene:
+                    var inst = scene.instantiate()
+                    inst.position = Vector2(x_offset + x*200 + 100, y_offset + y*150 + 100)
+                    add_child(inst)
+                    x += 1
+                    if x > 3:
+                        x = 0
+                        y += 1
+    # start recursion at templates root
+    spawn_from_dir("res://ComponentLibrary/Packs/VFX/Templates", 0, 0)
+    # also spawn one demo effect using sample impact component if exists
+    var comp_path = "res://ComponentLibrary/Packs/VFX/Components/impact_vfx_component.gd"
+    if ResourceLoader.exists(comp_path):
+        var comp = preload(comp_path).new()
+        add_child(comp)
+        comp.play_at(Vector2(300,200))
